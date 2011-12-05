b56aaac8227dd1263445963ea55774d5aac43893
Quinn Heiner <quinnyboy02@hotmail.com>
2011-12-04 22:41:29 -0700

    Final version of project for presentation

--- recipease_ASP4_version/Recipease/Shopping/index.aspx.cs
+++ recipease_ASP4_version/Recipease/Shopping/index.aspx.cs
@@ -12,9 +12,11 @@ public partial class Shopping_index : System.Web.UI.Page
         string meal_owner = User.Identity.Name.ToString(); // get UN for query string param
 
         // add current user to url for dynamic shopping list generation
-        if (Request.Url.ToString() == "http://localhost/Recipease/Shopping/index.aspx")
+        if (Request.Url.ToString() == "http://localhost/Recipease/Shopping/index.aspx" ||
+            Request.Url.ToString() == "http://161.28.118.37/Recipease/Shopping/index.aspx" ||
+            Request.Url.ToString() == "http://info3420-12/Recipease/Shopping/index.aspx")
         {
-                string url = "http://localhost/Recipease/Shopping/index.aspx?meal_owner=" + meal_owner;
+            string url = "http://161.28.118.37/Recipease/Shopping/index.aspx?meal_owner=" + meal_owner;
                 Response.Redirect(url);
         }
     }
